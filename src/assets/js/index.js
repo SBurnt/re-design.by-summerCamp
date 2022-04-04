722bf9655ecc54aa7530ab5d375fc93363ce8692
SBurnt <zip2004zip@gmail.com>
2022-04-05 00:56:15 +0300

    feat: add section activity

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,7 +2,7 @@ import 'core-js/stable';
 import './lazyload';
 import './header';
 // import './modals';
-// import './swiper';
+import './swiper';
 // import './validate-form';
 // import './accordion';
 // import './scrollToBlock';
