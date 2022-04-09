502e69194f3e5b2d8c3154f60057cc940093cd22
SBurnt <zip2004zip@gmail.com>
2022-04-09 17:02:31 +0300

    feat: add section format

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -2,10 +2,10 @@
 import Swiper from '../../../node_modules/swiper/swiper-bundle';
 
 const sliderActivity = document.querySelector('.js-slider-activity-init');
+const sliderTimetable = document.querySelector('.js-slider-timetable-init');
 
 if (sliderActivity) {
 	const swiper = new Swiper(sliderActivity, {
-		// loop: true,
 		slidesPerView: 'auto',
 		freeMode: true,
 		freeModeSticky: true,
@@ -15,6 +15,49 @@ if (sliderActivity) {
 		autoplay: {
 			delay: 2000,
 			disableOnInteraction: false,
+		},
+	});
+}
+
+if (sliderTimetable) {
+	const swiper = new Swiper(sliderTimetable, {
+		slidesPerView: 1.3,
+		spaceBetween: 15,
+		freeModeSticky: true,
+		grabCursor: true,
+		pauseOnMouseEnter: true,
+		speed: 800,
+		autoplay: {
+			delay: 2000,
+			disableOnInteraction: true,
+			pauseOnMouseEnter: true,
+		},
+		breakpoints: {
+			// when window width is >= 375px
+			375: {
+				slidesPerView: 1.4,
+				spaceBetween: 15,
+			},
+			768: {
+				slidesPerView: 2.4,
+				spaceBetween: 15,
+			},
+			1024: {
+				slidesPerView: 2.8,
+				spaceBetween: 30,
+			},
+			1280: {
+				slidesPerView: 3.5,
+				spaceBetween: 30,
+			},
+			1490: {
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
+			1920: {
+				slidesPerView: 4,
+				spaceBetween: 30,
+			},
 		},
 	});
 }
